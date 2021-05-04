5b746f690b116f7f7f838f3708e4216de2a468b6
dosimpact <ypd03008@gmail.com>
2021-05-04 18:06:37 +0900

    file dir

---
+++ client/src/routes/Home/HomeC.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HomeP from "./HomeP";
+
+const HomeC = () => {
+  return <HomeP />;
+};
+
+export default HomeC;
---
+++ client/src/routes/Home/HomeP.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TabExample from "components/TabExample";
+
+const HomeP = () => {
+  return (
+    <div>
+      <TabExample />
+    </div>
+  );
+};
+
+export default HomeP;
