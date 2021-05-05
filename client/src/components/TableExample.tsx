2f982ce78757209c16ec87952dcc03aff9dc8612
Hyun-juhee <juhe1998@naver.com>
2021-05-05 21:02:02 +0900

    feat(TableExample)

---
+++ client/src/components/TableExample.tsx
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+import { Table } from 'antd';
+import 'antd/dist/antd.css';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Price',
+    dataIndex: 'chinese',
+    sorter: {
+      compare: (a, b) => a.chinese - b.chinese,
+      multiple: 3,
+    },
+  },
+  {
+    title: '전일대비',
+    dataIndex: 'math',
+    sorter: {
+      compare: (a, b) => a.math - b.math,
+      multiple: 2,
+    },
+    render: (text: number) => (text>0) ? <a style={{color: '#05c545'}}>{text}</a> : <a style={{color: '#ff3d00'}}>{text}</a>,
+  },
+  {
+    title: 'Transaction Amount',
+    dataIndex: 'english',
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
+    render: (text: number) => <a style={{color: '#05c545'}}>{text}</a>,
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    chinese: 98,
+    math: 60,
+    english: 70,
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    chinese: 98,
+    math: -66,
+    english: 89,
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    chinese: 98,
+    math: 90,
+    english: 70,
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    chinese: 88,
+    math: 99,
+    english: 89,
+  },
+];
+
+function onChange(pagination, filters, sorter, extra) {
+  console.log('params', pagination, filters, sorter, extra);
+}
+const TableExample = () => (
+  <div>
+    <Table columns={columns} dataSource={data} onChange={onChange} />
+  </div>
+);
+
+export default TableExample;
--- client/src/routes/Home/HomeP.tsx
+++ client/src/routes/Home/HomeP.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import TabExample from "components/TabExample";
 import ExampleStyled from "components/ExampleStyled";
 import TradingView from "components/TradingView";
+import TableExample from "components/TableExample";
 
 const HomeP = () => {
   return (
@@ -9,6 +10,7 @@ const HomeP = () => {
       <TradingView />
       <TabExample />
       <ExampleStyled text="hello there" color={"BLUE"} />
+      <TableExample/>
     </div>
   );
 };
