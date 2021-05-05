a4da880d46461e4fbe12ba673bd8f8bfa4aba33a
dosimpact <ypd03008@gmail.com>
2021-05-05 14:02:15 +0900

    TradingView

--- client/src/components/TradingView.tsx
+++ client/src/components/TradingView.tsx
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
 
-interface ITradingView {}
+//https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/class_components/
 
-export default class TradingView extends Component<{}, ITradingView> {
+// state typing
+interface STradingView {}
+
+// props typing
+interface PTradingView {
+  // rootRef:React.RefObject<HTMLDivElement>
+}
+
+export default class TradingView extends Component<PTradingView, STradingView> {
+  rootRef: React.RefObject<HTMLDivElement>;
   constructor(props) {
     super(props);
+    this.rootRef = React.createRef<HTMLDivElement>();
   }
-  componentDidMount() {}
+
+  componentDidMount() {
+    new window.TradingView.widget({
+      width: 980,
+      height: 610,
+      symbol: "NASDAQ:AAPL",
+      interval: "D",
+      timezone: "Etc/UTC",
+      theme: "light",
+      style: "1",
+      locale: "en",
+      toolbar_bg: "#f1f3f6",
+      enable_publishing: false,
+      allow_symbol_change: true,
+      container_id: "tradingview_a521c",
+    });
+  }
   componentWillUnmount() {}
   render() {
-    return <div></div>;
+    return (
+      <div ref={this.rootRef}>
+        <div className="tradingview-widget-container">
+          <div id="tradingview_a521c"></div>
+        </div>
+      </div>
+    );
   }
 }
--- client/src/routes/Home/HomeP.tsx
+++ client/src/routes/Home/HomeP.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TabExample from "components/TabExample";
 import ExampleStyled from "components/ExampleStyled";
+import TradingView from "components/TradingView";
 
 const HomeP = () => {
   return (
     <div>
+      <TradingView />
       <TabExample />
       <ExampleStyled text="hello there" color={"BLUE"} />
     </div>
