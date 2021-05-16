858ddc0e6d63592406f4395fae0820e628fd796b
dosimpact <ypd03008@gmail.com>
2021-05-16 18:46:35 +0900

    setInterval fetchError

--- client/src/api/TableData.tsx
+++ client/src/api/TableData.tsx
@@ -1,12 +1,17 @@
 import axios from 'axios';
+
+
 export const server_check = async () => {
     try {
         const { data } = await axios.get(`http://133.186.229.72:4100`);
-        console.log(data);
+        // console.log(data);
+        return data
     } catch (e) {
         throw e;
     }
 };
+
+
 export const get_current_price = async () => {
     try {
         const { data } = await axios.get(`http://133.186.229.72:4100/get_current_price`);
@@ -15,3 +20,18 @@ export const get_current_price = async () => {
         throw e;
     }
 };
+
+export type CurrentPrice =  {
+    opening_price: string,
+    closing_price: string,
+    min_price: string,
+    max_price: string,
+    units_traded: string,
+    acc_trade_value: string,
+    prev_closing_price: string,
+    units_traded_24H: string,
+    acc_trade_value_24H: string,
+    fluctate_24H:string,
+    fluctate_rate_24H:string,
+    BULL_5: number | undefined
+}
--- client/src/components/TableExample.tsx
+++ client/src/components/TableExample.tsx
@@ -2,7 +2,7 @@ import { Table } from "antd";
 import "antd/dist/antd.css";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { server_check, get_current_price } from "../api/TableData";
+import { server_check, get_current_price, CurrentPrice } from "../api/TableData";
 
 const columns = [
   {
@@ -92,47 +92,59 @@ const TableExample: React.FunctionComponent<ITableExample> = ({
     bull: string;
   };
   const [dataSource, setData] = useState<Row[]>([]);
+  const [fetchError,setFetchError ] = useState<boolean>(false);
 
   useEffect(() => {
-    (async () => {
-      await server_check();
-      const data = await get_current_price();
-      let arr: string[] = [];
-      for (let key in data) {
-        arr.push(key);
-      }
-      
-      console.log(arr.length,data.length);
 
-      let temp: Row[] = [];
+    const fetchData = async  ()=>{
+      try {
+        await server_check(); 
 
-      for (let i = 0; i < arr.length; i++) {
-        let compare_number: number =
-          ((data[arr[i]].closing_price - data[arr[i]].prev_closing_price) /
-            data[arr[i]].prev_closing_price) *
-          100;
-        let compare: string = compare_number.toFixed(2);
-        let bull: string = data[arr[i]].BULL_5
-          ? data[arr[i]].BULL_5.toFixed(2)
-          : "";
-        let obj: Row = {
-          key: `${i}`,
-          name: arr[i],
-          show_price: data[arr[i]].closing_price.replace(
-            /\B(?=(\d{3})+(?!\d))/g,
-            ","
-          ),
-          show_prev_compare:
-            compare.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "%",
-          show_bull: bull.replace(/\B(?=(\d{3})+(?!\d))/g, ","),
-          price: data[arr[i]].closing_price,
-          prev_compare: compare_number,
-          bull: data[arr[i]].BULL_5,
-        };
-        temp.push(obj);
+        const data:[CurrentPrice] = await get_current_price();
+        let arr: string[] = [];
+        for (let key in data) {
+          arr.push(key);
+        }
+        console.log("fetch data ",arr.length,data.length);
+        let temp: Row[] = [];
+        for (let i = 0; i < arr.length; i++) {
+          let compare_number: number =
+            ((data[arr[i]].closing_price - data[arr[i]].prev_closing_price) /
+              data[arr[i]].prev_closing_price) *
+            100;
+          let compare: string = compare_number.toFixed(2);
+          let bull: string = data[arr[i]].BULL_5
+            ? data[arr[i]].BULL_5.toFixed(2)
+            : "";
+          let obj: Row = {
+            key: `${i}`,
+            name: arr[i],
+            show_price: data[arr[i]].closing_price.replace(
+              /\B(?=(\d{3})+(?!\d))/g,
+              ","
+            ),
+            show_prev_compare:
+              compare.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "%",
+            show_bull: bull.replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+            price: data[arr[i]].closing_price,
+            prev_compare: compare_number,
+            bull: data[arr[i]].BULL_5,
+          };
+          temp.push(obj);
+        }
+        
+        setData(temp);
+      } catch (error) {
+        console.log("fetch error")
+        setFetchError(true)
       }
-      setData(temp);
-    })();
+    }
+
+    // fetchData()
+    const interval_fetch = setInterval(fetchData,1000)
+    return ()=>{
+      clearInterval(interval_fetch)
+    }
   }, []);
 
   return (
@@ -153,7 +165,7 @@ const TableExample: React.FunctionComponent<ITableExample> = ({
           };
         }}
         columns={columns}
-        dataSource={dataSource}
+        dataSource={ fetchError ? [] : dataSource }
         pagination={{ pageSize: 30 }}
       />
     </TableStyled>
