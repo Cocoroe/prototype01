348b1ce03fcf3d29421e40e93f03887f16043b28
dosimpact <ypd03008@gmail.com>
2021-09-03 13:01:33 +0000

    .

--- server_node/index.js
+++ server_node/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path")
-
+const port = process.env.PORT || 80
 app.use(express.static(path.join(__dirname,"../client/build")))
 
 app.get("/",function(req,res){
     res.sendFile(path.join(__dirname,"./client/build","index.html"))
 })
 
-app.listen(process.env.PORT || 80);
-console.log("✔ Server is starting...")
+app.listen(port);
+console.log(`✔ NodeJS Server is starting... (PORT=${port})`)
